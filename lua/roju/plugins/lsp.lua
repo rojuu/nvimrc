e3e7ad37f5c288a5121b48fb85b7b35ff1d69f88
Roni Juppi <roju269@gmail.com>
2024-04-12 22:57:19 +0300

    Use simpler ts server
    The ts tools one had some annoying kinks, like if I press K to
    show the hover info, it would move my cursor inside the hover
    tooltip. In the normal LSP, it will normally just show the hover
    and go inside the tooltip if i press K again.

--- lua/roju/plugins/lsp.lua
+++ lua/roju/plugins/lsp.lua
@@ -69,6 +69,7 @@ return { -- LSP Configuration & Plugins
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 
+      tsserver = {},
       tailwindcss = {},
 
       lua_ls = {

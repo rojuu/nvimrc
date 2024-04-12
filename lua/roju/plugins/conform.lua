840a36856c9fe04b66c5dc8ad612ca744802539e
Roni Juppi <roju269@gmail.com>
2024-04-12 22:56:53 +0300

    Make formatting very fast in JS/TS

--- lua/roju/plugins/conform.lua
+++ lua/roju/plugins/conform.lua
@@ -25,16 +25,16 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { "stylua" },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      javascriptreact = { "prettierd" },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
       -- javascript = { { "prettierd", "prettier" } },
-      javascript = { { "prettierd", "prettier", "npx prettier" } },
-      javascriptreact = { { "prettierd", "prettier", "npx prettier" } },
-      typescript = { { "prettierd", "prettier", "npx prettier" } },
-      typescriptreact = { { "prettierd", "prettier", "npx prettier" } },
     },
   },
 }
--- lua/roju/plugins/lsp.lua
+++ lua/roju/plugins/lsp.lua
@@ -100,6 +100,7 @@ return { -- LSP Configuration & Plugins
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       "stylua", -- Used to format Lua code
+      "prettierd", -- Used to format Javascript code
     })
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 

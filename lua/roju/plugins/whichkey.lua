cfcf243201c041338cbee1323acf939b17703d06
Roni Juppi <roju269@gmail.com>
2024-04-13 22:51:34 +0300

    Disable whichkey for now

--- lua/roju/plugins/whichkey.lua
+++ lua/roju/plugins/whichkey.lua
@@ -1,16 +1,20 @@
 return { -- Useful plugin to show you pending keybinds.
-  "folke/which-key.nvim",
-  event = "VimEnter", -- Sets the loading event to "VimEnter"
-  config = function() -- This is the function that runs, AFTER loading
-    require("which-key").setup()
-
-    -- Document existing key chains
-    --require("which-key").register {
-    --  ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-    --  ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-    --  ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-    --  ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-    --  ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-    --}
-  end,
+  --   "folke/which-key.nvim",
+  --   event = "VimEnter", -- Sets the loading event to "VimEnter"
+  --   opts = {
+  --     plugins = {
+  --       marks = true, -- shows a list of your marks on ' and `
+  --       registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+  --       presets = {
+  --         operators = false, -- adds help for operators like d, y, ...
+  --         motions = false, -- adds help for motions
+  --         text_objects = false, -- help for text objects triggered after entering an operator
+  --         windows = false, -- default bindings on <c-w>
+  --         nav = false, -- misc bindings to work with windows
+  --         z = true, -- bindings for folds, spelling and others prefixed with z
+  --         g = false, -- bindings for prefixed with g
+  --       },
+  --     },
+  --     operators = {},
+  --   },
 }

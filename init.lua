61a2d95d46669d52c39737251e882bdc55f6b4ee
Roni Juppi <roju269@gmail.com>
2024-04-13 21:27:46 +0300

    Bring back the tab viz

--- init.lua
+++ init.lua
@@ -29,9 +29,8 @@ vim.opt.splitbelow = false
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = false
-vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
--- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.opt.showbreak = "\\"
 
 vim.opt.inccommand = "split"

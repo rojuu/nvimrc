8caa3bb987fb5be680386eac1f495d505f67951c
Roni Juppi <roju269@gmail.com>
2024-06-25 22:39:36 +0300

    Add keybind to toggle neotree

--- lua/roju/plugins/neotree.lua
+++ lua/roju/plugins/neotree.lua
@@ -6,14 +6,19 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  opts = {
-    filesystem = {
-      follow_current_file = {
-        enabled = true, -- This will find and focus the file in the active buffer every time
-        --               -- the current file is changed while the tree is open.
-        leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+  config = function()
+    local neotree = require("neo-tree")
+    neotree.setup({
+      filesystem = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+        hijack_netrw_behavior = "disabled",
       },
-      hijack_netrw_behavior = "disabled",
-    },
-  },
+    })
+
+    vim.keymap.set("n", "<leader>b", ":Neotree toggle<CR>")
+  end,
 }

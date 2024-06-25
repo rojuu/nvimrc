0078280bc583a9ec680ff08d71c73e6161f5891c
Roni Juppi <roju269@gmail.com>
2024-06-25 22:15:10 +0300

    Let's try out neotree as an experiment

---
+++ lua/roju/plugins/neotree.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+      hijack_netrw_behavior = "disabled",
+    },
+  },
+}
